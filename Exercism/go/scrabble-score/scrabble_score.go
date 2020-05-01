d9131c2155796fdee3d333d7a6e5c4da20980a41
Marek Chowaniok <marek.chowaniok@gmail.com>
2020-05-01 20:05:58 +0200

    refactored code

--- Exercism/go/scrabble-score/scrabble_score.go
+++ Exercism/go/scrabble-score/scrabble_score.go
@@ -1,34 +1,32 @@
 package scrabble
 
-import "strings"
+import (
+	"strings"
+)
+
+var data = map[rune]string{
+	1:  "AEIOULNRST",
+	2:  "DG",
+	3:  "BCMP",
+	4:  "FHVWY",
+	5:  "K",
+	8:  "JX",
+	10: "QZ",
+}
 
 // Score function for scramble
 func Score(word string) int {
-
 	result := 0
-	for i := 0; i < len(word); i++ {
-		result += getScore(strings.ToUpper(string(word[i])))
-	}
 
-	return result
-}
+	for _, i := range strings.ToUpper(word) {
+		// fmt.Printf("Size: %d\nType: %s\nCharacter: %c\n", unsafe.Sizeof(aa), reflect.TypeOf(aa), aa)
 
-func getScore(character string) int {
-	data := map[int]string{
-		1:  "AEIOULNRST",
-		2:  "DG",
-		3:  "BCMP",
-		4:  "FHVWY",
-		5:  "K",
-		8:  "JX",
-		10: "QZ",
-	}
-
-	result := 0
-	for key, value := range data {
-		if strings.Contains(value, character) {
-			result = key
+		for key, value := range data {
+			if strings.Contains(value, string(rune(i))) {
+				result += int(key)
+			}
 		}
 	}
+
 	return result
 }
